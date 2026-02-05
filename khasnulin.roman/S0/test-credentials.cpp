38ff0e990d4f141767626be526dead04227dce26
Spider-has <romanh998@gmail.com>
2026-02-05 19:22:07 +0300

    test suite

--- khasnulin.roman/S0/test-credentials.cpp
+++ khasnulin.roman/S0/test-credentials.cpp
@@ -2,9 +2,20 @@
 #include <boost/test/unit_test.hpp>
 #include <sstream>
 
+BOOST_AUTO_TEST_SUITE(credentials_suite)
+
 BOOST_AUTO_TEST_CASE(credential_test)
 {
   std::ostringstream out;
   khasnulin::out_credentials(out);
   BOOST_TEST(out.str() == "khasnulin.roman");
 }
+
+BOOST_AUTO_TEST_CASE(yacredential_test)
+{
+  std::ostringstream out;
+  khasnulin::out_credentials(out);
+  BOOST_TEST(out.str() == "khasnulin.roman");
+}
+
+BOOST_AUTO_TEST_SUITE_END()

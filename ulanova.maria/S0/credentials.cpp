9fa023983159ed374880a5430616bc237753e3c5
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-02-08 16:52:09 +0300

    out_credentials implementation

---
+++ ulanova.maria/S0/credentials.cpp
@@ -0,0 +1,7 @@
+#include "credentials.hpp"
+#include <iostream>
+
+std::ostream & ulanova::out_credentials(std::ostream & out)
+{
+    return out << "ulanova.maria";
+}
--- ulanova.maria/S0/main.cpp
+++ ulanova.maria/S0/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <credentials.hpp>
+#include "credentials.hpp"
 
 int main()
 {

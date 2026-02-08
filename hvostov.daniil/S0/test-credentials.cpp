90081eb7ff83ebf49d2fe2a1e5778f6425cd57c5
PodySIN <daniil2007xboctob@gmail.com>
2026-02-08 17:23:22 +0300

    credentials test

--- hvostov.daniil/S0/test-credentials.cpp
+++ hvostov.daniil/S0/test-credentials.cpp
@@ -1,8 +1,10 @@
-#define BOOST_TEST_MODULE S0
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+#include "credentials.hpp"
 
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
-  int i = 1;
-  BOOST_TEST(i == 2);
+  std::ostringstream out;
+  hvostov::out_credentials(out);
+  BOOST_TEST(out.str() == "hvostov.daniil");
 }

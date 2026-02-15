9ba8e4d834126ffb6fd477c9dc2a2d148b0953c0
Dima <mit.levkin@vk.com>
2026-02-15 22:43:45 +0300

    Add credentials test suite and extra test

--- levkin.dima/S0/test-credentials.cpp
+++ levkin.dima/S0/test-credentials.cpp
@@ -1,9 +1,21 @@
 #include "credentials.hpp"
 #include <boost/test/unit_test.hpp>
 #include <sstream>
+
+BOOST_AUTO_TEST_SUITE(credentials_suite)
+
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
   std::ostringstream out;
   levkin::out_credentials(out);
   BOOST_TEST(out.str() == "levkin.dima");
 }
+
+BOOST_AUTO_TEST_CASE(yet_another_credentials_test)
+{
+  std::ostringstream out;
+  levkin::out_credentials(out);
+  BOOST_TEST(out.str()[0] == 'l');
+}
+
+BOOST_AUTO_TEST_SUITE_END()

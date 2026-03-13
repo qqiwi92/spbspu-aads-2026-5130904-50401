792b0a7cf58f0854f53cf2104899f8870798e6ae
Dima <168963586+qqiwi92@users.noreply.github.com>
2026-03-13 23:04:41 +0300

    add try/catch in main() to return right codes

--- levkin.dima/S1/main.cpp
+++ levkin.dima/S1/main.cpp
@@ -1,11 +1,26 @@
+#include <exception>
 #include <iostream>
+#include <stdexcept>
 #include "io.hpp"
 
 using namespace levkin;
 int main()
 {
-  Data data;
-  readData(std::cin, data);
-  printNames(std::cout, data);
-  printTransposed(std::cout, data);
+  try {
+    Data data;
+
+    readData(std::cin, data);
+
+    if (data.cbegin() != data.cend()) {
+      levkin::printNames(std::cout, data);
+      levkin::printTransposed(std::cout, data);
+    } else {
+      std::cout << "0\n";
+    }
+  } catch (const std::overflow_error &e) {
+    std::cerr << e.what() << "\n";
+    return 1;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << "\n";
+  }
 }

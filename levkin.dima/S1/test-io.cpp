3530ad551711282cccc1a7cc2610362cb2b08c4b
Dima <168963586+qqiwi92@users.noreply.github.com>
2026-03-13 16:47:51 +0300

    add output tests for prints
    printSums(Out&, const Data& data);
    printTransposed(Out&, const Data& data);

--- levkin.dima/S1/test-io.cpp
+++ levkin.dima/S1/test-io.cpp
@@ -21,3 +21,48 @@ BOOST_AUTO_TEST_CASE(read_data_test)
   BOOST_CHECK_EQUAL(it->first, "third");
   BOOST_CHECK(it->second.begin() == it->second.end());
 }
+
+BOOST_AUTO_TEST_CASE(output_simulator_test)
+{
+  Data data;
+
+  Lst l1;
+  l1.pushBack(1);
+  l1.pushBack(1);
+  l1.pushBack(1);
+  Lst l2;
+  l2.pushBack(2);
+  l2.pushBack(2);
+  l2.pushBack(2);
+  l2.pushBack(2);
+  Lst l3;
+  Lst l4;
+  l4.pushBack(4);
+  l4.pushBack(4);
+
+  data.pushBack({"first", l1});
+  data.pushBack({"second", l2});
+  data.pushBack({"third", l3});
+  data.pushBack({"fourth", l4});
+
+  std::stringstream outTransposed;
+
+  for (auto it = data.begin(); it != data.end(); ++it) {
+    outTransposed << it->first << ((++it) == data.end() ? "" : " ");
+  }
+  outTransposed << "\n";
+
+  printTransposed(outTransposed, data);
+
+  std::string expectedOutput = "first second third fourth\n"
+                               "1 2 4\n"
+                               "1 2 4\n"
+                               "1 2\n"
+                               "2\n";
+
+  BOOST_CHECK_EQUAL(outTransposed.str(), expectedOutput);
+
+  std::stringstream outSums;
+
+  BOOST_CHECK_EQUAL(outSums.str(), "7 7 3 2\n");
+}

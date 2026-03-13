3c16be163450823ee42ca47a97fe3b78ee65a8fe
Dima <168963586+qqiwi92@users.noreply.github.com>
2026-03-13 16:39:55 +0300

    implemented readData(In& in, Data& data)

--- levkin.dima/S1/io.cpp
+++ levkin.dima/S1/io.cpp
@@ -1,3 +1,39 @@
-namespace levkin {
-  
+#include "io.hpp"
+#include <cstdio>
+namespace levkin
+{
+  In& readData(In& in, Data& data)
+  {
+    while (in) {
+      String name;
+      if (!(in >> name))
+        break;
+
+      Lst currentList;
+      while (in.peek() == ' ' || in.peek() == '\t') {
+        in.ignore();
+      }
+      while (in.peek() != '\n' && in.peek() != '\r' && in.peek() != EOF) {
+        size_t digit = 0;
+        if (in >> digit) {
+          currentList.pushBack(digit);
+        } else {
+          in.clear();
+          break;
+        }
+        while (in.peek() == ' ' || in.peek() == '\t') {
+          in.ignore();
+        }
+      }
+      Pair pair(name, currentList);
+      data.pushBack(pair);
+
+      if (in.peek() == '\n' || in.peek() == '\r') {
+        in.ignore();
+      }
+    }
+    return in;
+  }
+  Out& printTransposed(Out&, const Data& data);
+  Out& printSums(Out&, const Data& data);
 }

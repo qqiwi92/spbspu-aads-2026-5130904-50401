d98f12dd5611b0099453c73cc8f39bfa6de717b9
Dmitry Levkin <mit.levkin@vk.com>
2026-03-30 12:26:52 +0300

    add implementations of stack and queue

--- levkin.dima/common/queue.hpp
+++ levkin.dima/common/queue.hpp
@@ -1,8 +1,27 @@
+#ifndef MY_QUEUE
+#define MY_QUEUE
+#include "list.hpp"
 namespace levkin {
   template < typename T > class Queue
   {
+
+  private:
+    levkin::List< T > list_;
+
   public:
+    Queue() : list_(List< T >{}) {}
     void push(T rhs);
     T drop();
   };
+  template < class T > T levkin::Queue< T >::drop()
+  {
+    auto iter = list_.cbegin();
+    T val = *iter;
+    list_.popFront();
+    return val;
+  }
+  template < class T > void levkin::Queue< T >::push(T v) { list_.pushBack(v); }
+
 }
+
+#endif
--- levkin.dima/common/stack.hpp
+++ levkin.dima/common/stack.hpp
@@ -1,8 +1,27 @@
+#ifndef MY_STACK
+#define MY_STACK
+#include "list.hpp"
 namespace levkin {
   template < typename T > class Stack
   {
+
+  private:
+    levkin::List< T > list_;
+
   public:
+    Stack() : list_(List< T >{}) {}
     void push(T rhs);
     T drop();
   };
+  template < class T > T levkin::Stack< T >::drop()
+  {
+    auto iter = list_.cbegin();
+    T val = *iter;
+    list_.popFront();
+    return val;
+  }
+  template < class T > void levkin::Stack< T >::push(T v) { list_.pushFront(v); }
+
 }
+
+#endif

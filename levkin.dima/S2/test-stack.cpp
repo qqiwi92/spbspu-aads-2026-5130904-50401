598529f938b796c942c6dce6e4f94cd53734265e
Dmitry Levkin <mit.levkin@vk.com>
2026-03-30 11:48:12 +0300

    add test for stack

---
+++ levkin.dima/S2/test-stack.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/tools/old/interface.hpp>
+#include <boost/test/unit_test.hpp>
+#include "stack.hpp"
+using namespace levkin;
+BOOST_AUTO_TEST_CASE(constructor_test)
+{
+  levkin::Stack< int > a{};
+  a.push(1);
+  a.push(2);
+  a.push(3);
+  BOOST_CHECK_EQUAL(a.drop(), 3);
+  BOOST_CHECK_EQUAL(a.drop(), 2);
+  BOOST_CHECK_EQUAL(a.drop(), 1);
+}

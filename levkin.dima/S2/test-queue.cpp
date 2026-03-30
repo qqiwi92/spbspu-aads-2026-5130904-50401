394aa312f3b246c6821be9ebaece00c3570421c9
Dmitry Levkin <mit.levkin@vk.com>
2026-03-30 12:48:29 +0300

    tests for pop() and front()

--- levkin.dima/S2/test-queue.cpp
+++ levkin.dima/S2/test-queue.cpp
@@ -1,8 +1,9 @@
+#include "queue.hpp"
 #include <boost/test/tools/old/interface.hpp>
 #include <boost/test/unit_test.hpp>
-#include "queue.hpp"
+
 using namespace levkin;
-BOOST_AUTO_TEST_CASE(queue_test)
+BOOST_AUTO_TEST_CASE(queue_drop_push_test)
 {
   levkin::Queue< int > a{};
   a.push(1);
@@ -10,5 +11,18 @@ BOOST_AUTO_TEST_CASE(queue_test)
   a.push(3);
   BOOST_CHECK_EQUAL(a.drop(), 1);
   BOOST_CHECK_EQUAL(a.drop(), 2);
+  BOOST_CHECK_EQUAL(a.drop(), 3);
+}
+
+BOOST_AUTO_TEST_CASE(queue_pop_front_test)
+{
+  levkin::Queue< int > a{};
+  a.push(1);
+  a.push(2);
+  a.push(3);
+  BOOST_CHECK_EQUAL(a.front(), 1);
+  BOOST_CHECK_EQUAL(a.front(), 1);
+  a.pop();
+  a.pop();
   BOOST_CHECK_EQUAL(a.drop(), 3);
 }
--- levkin.dima/S2/test-stack.cpp
+++ levkin.dima/S2/test-stack.cpp
@@ -1,8 +1,9 @@
+#include "stack.hpp"
 #include <boost/test/tools/old/interface.hpp>
 #include <boost/test/unit_test.hpp>
-#include "stack.hpp"
+
 using namespace levkin;
-BOOST_AUTO_TEST_CASE(stack_test)
+BOOST_AUTO_TEST_CASE(stack_drop_push_test)
 {
   levkin::Stack< int > a{};
   a.push(1);
@@ -10,5 +11,18 @@ BOOST_AUTO_TEST_CASE(stack_test)
   a.push(3);
   BOOST_CHECK_EQUAL(a.drop(), 3);
   BOOST_CHECK_EQUAL(a.drop(), 2);
+  BOOST_CHECK_EQUAL(a.drop(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(stack_pop_front_test)
+{
+  levkin::Stack< int > a{};
+  a.push(1);
+  a.push(2);
+  a.push(3);
+  BOOST_CHECK_EQUAL(a.front(), 3);
+  BOOST_CHECK_EQUAL(a.front(), 3);
+  a.pop();
+  a.pop();
   BOOST_CHECK_EQUAL(a.drop(), 1);
 }

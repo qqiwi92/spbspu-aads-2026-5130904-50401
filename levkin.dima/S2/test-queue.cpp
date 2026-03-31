0a69278c99c68206b9920debc087599c90f2b158
Dmitry Levkin <mit.levkin@vk.com>
2026-03-31 08:46:41 +0300

    tests for edge cases

--- levkin.dima/S2/test-queue.cpp
+++ levkin.dima/S2/test-queue.cpp
@@ -1,28 +1,44 @@
 #include "queue.hpp"
-#include <boost/test/tools/old/interface.hpp>
 #include <boost/test/unit_test.hpp>
+#include <string>
+#include <stdexcept>
 
 using namespace levkin;
-BOOST_AUTO_TEST_CASE(queue_drop_push_test)
+
+
+BOOST_AUTO_TEST_CASE(queue_basic_operations)
 {
-  levkin::Queue< int > a{};
-  a.push(1);
-  a.push(2);
-  a.push(3);
-  BOOST_CHECK_EQUAL(a.drop(), 1);
-  BOOST_CHECK_EQUAL(a.drop(), 2);
-  BOOST_CHECK_EQUAL(a.drop(), 3);
+  Queue< int > q;
+  BOOST_CHECK(q.empty());
+  BOOST_CHECK_EQUAL(q.size(), 0);
+
+  q.push(10);
+  q.push(20);
+
+  BOOST_CHECK(!q.empty());
+  BOOST_CHECK_EQUAL(q.size(), 2);
+  BOOST_CHECK_EQUAL(q.front(), 10);
+
+  BOOST_CHECK_EQUAL(q.drop(), 10);
+  BOOST_CHECK_EQUAL(q.size(), 1);
+  BOOST_CHECK_EQUAL(q.front(), 20);
 }
 
-BOOST_AUTO_TEST_CASE(queue_pop_front_test)
+BOOST_AUTO_TEST_CASE(queue_exception_safety)
 {
-  levkin::Queue< int > a{};
-  a.push(1);
-  a.push(2);
-  a.push(3);
-  BOOST_CHECK_EQUAL(a.front(), 1);
-  BOOST_CHECK_EQUAL(a.front(), 1);
-  a.pop();
-  a.pop();
-  BOOST_CHECK_EQUAL(a.drop(), 3);
+  Queue< int > q;
+  BOOST_CHECK_THROW(q.front(), std::out_of_range);
+  BOOST_CHECK_THROW(q.drop(), std::out_of_range);
+
+  BOOST_CHECK_NO_THROW(q.pop());
 }
+
+BOOST_AUTO_TEST_CASE(queue_move_semantics)
+{
+  Queue< std::string > q;
+  std::string s = "heavy_object";
+  q.push(std::move(s));
+  BOOST_CHECK_EQUAL(q.front(), "heavy_object");
+  BOOST_CHECK(s.empty());
+}
+
--- levkin.dima/S2/test-stack.cpp
+++ levkin.dima/S2/test-stack.cpp
@@ -1,28 +1,48 @@
 #include "stack.hpp"
-#include <boost/test/tools/old/interface.hpp>
 #include <boost/test/unit_test.hpp>
+#include <stdexcept>
 
 using namespace levkin;
-BOOST_AUTO_TEST_CASE(stack_drop_push_test)
+BOOST_AUTO_TEST_CASE(stack_lifo_order)
 {
-  levkin::Stack< int > a{};
-  a.push(1);
-  a.push(2);
-  a.push(3);
-  BOOST_CHECK_EQUAL(a.drop(), 3);
-  BOOST_CHECK_EQUAL(a.drop(), 2);
-  BOOST_CHECK_EQUAL(a.drop(), 1);
+    Stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    BOOST_CHECK_EQUAL(s.top(), 3);
+    s.pop();
+    BOOST_CHECK_EQUAL(s.top(), 2);
+    BOOST_CHECK_EQUAL(s.drop(), 2);
+    BOOST_CHECK_EQUAL(s.top(), 1);
 }
 
-BOOST_AUTO_TEST_CASE(stack_pop_front_test)
+BOOST_AUTO_TEST_CASE(stack_empty_behavior)
 {
-  levkin::Stack< int > a{};
-  a.push(1);
-  a.push(2);
-  a.push(3);
-  BOOST_CHECK_EQUAL(a.front(), 3);
-  BOOST_CHECK_EQUAL(a.front(), 3);
-  a.pop();
-  a.pop();
-  BOOST_CHECK_EQUAL(a.drop(), 1);
+    Stack<double> s;
+    BOOST_CHECK(s.empty());
+    
+    BOOST_CHECK_THROW(s.top(), std::out_of_range);
+    BOOST_CHECK_THROW(s.drop(), std::out_of_range);
+    
+    s.push(1.1);
+    s.pop();
+    BOOST_CHECK(s.empty());
+    BOOST_CHECK_THROW(s.top(), std::out_of_range);
 }
+
+BOOST_AUTO_TEST_CASE(stack_large_volume)
+{
+    Stack<int> s;
+    for(int i = 0; i < 1000; ++i) {
+        s.push(i);
+    }
+    BOOST_CHECK_EQUAL(s.size(), 1000);
+    BOOST_CHECK_EQUAL(s.top(), 999);
+    
+    for(int i = 0; i < 1000; ++i) {
+        s.pop();
+    }
+    BOOST_CHECK(s.empty());
+}
+

a3f748557a505ffa9a15e55f6a0fe19e08717c46
Dmitry Levkin <mit.levkin@vk.com>
2026-04-02 20:16:19 +0300

    tweaked flags

--- levkin.dima/S2/test-utils.cpp
+++ levkin.dima/S2/test-utils.cpp
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_CASE(to_digit)
   bool flag = 0;
   size_t result = toDigit(s, 0, s.size(), flag);
   BOOST_CHECK_EQUAL(result, 3234);
-  BOOST_CHECK_EQUAL(flag, 0);
+  BOOST_CHECK_EQUAL(flag, 1);
 }
 
 BOOST_AUTO_TEST_CASE(to_digit_corner_case_with_chars)
@@ -22,7 +22,7 @@ BOOST_AUTO_TEST_CASE(to_digit_corner_case_with_chars)
   bool flag = 0;
   size_t result = toDigit(s, 0, s.size(), flag);
   BOOST_CHECK_EQUAL(result, 0);
-  BOOST_CHECK_EQUAL(flag, 1);
+  BOOST_CHECK_EQUAL(flag, 0);
 }
 
 BOOST_AUTO_TEST_CASE(get_nxt_word)
@@ -33,15 +33,14 @@ BOOST_AUTO_TEST_CASE(get_nxt_word)
   BOOST_CHECK_EQUAL(getNextWord(s, 4), s.size());
 }
 
-
 BOOST_AUTO_TEST_CASE(shorten_operation)
 {
   std::string s = "+-*/**what?";
 
-  BOOST_CHECK_EQUAL(encodeOpOrThrow(s, 0, 1), '+');
-  BOOST_CHECK_EQUAL(encodeOpOrThrow(s, 1, 2), '-');
-  BOOST_CHECK_EQUAL(encodeOpOrThrow(s, 2, 3), '*');
-  BOOST_CHECK_EQUAL(encodeOpOrThrow(s, 3, 4), '/');
-  BOOST_CHECK_EQUAL(encodeOpOrThrow(s, 4, 6), '^');
-  BOOST_CHECK_THROW(encodeOpOrThrow(s, 6, s.size()),std::runtime_error);
+  BOOST_CHECK_EQUAL(encodeOpOrThrow(s, 0, 1)(2, 40), 42);
+  BOOST_CHECK_EQUAL(encodeOpOrThrow(s, 1, 2)(64, 21), 43);
+  BOOST_CHECK_EQUAL(encodeOpOrThrow(s, 2, 3)(11, 4), 44);
+  BOOST_CHECK_EQUAL(encodeOpOrThrow(s, 3, 4)(48, 4), 12);
+  BOOST_CHECK_EQUAL(encodeOpOrThrow(s, 4, 6)(2, 5), 32);
+  BOOST_CHECK_THROW(encodeOpOrThrow(s, 6, s.size()), std::runtime_error);
 }

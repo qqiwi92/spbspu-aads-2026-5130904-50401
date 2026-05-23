1abf848e9a6276285ab42d03fac723b9fe987115
Dmitry Levkin <mit.levkin@vk.com>
2026-05-23 16:30:58 +0300

    Implement file-based graph loading and command processing

--- levkin.dima/S3/main.cpp
+++ levkin.dima/S3/main.cpp
@@ -1,30 +1,81 @@
+#include <fstream>
 #include <iostream>
+#include <limits>
 #include <string>
+
 #include "commands.hpp"
+#include "graph.hpp"
+#include "hasher.hpp"
+#include "hashtable.hpp" 
+#include "vector.hpp"    
 
-int main()
+int main(int args, char ** argv)
 {
-  std::string current;
-  using namespace levkin;
-  Cmds cmds = getCmds();
-  Database db;
+  if (args != 2) {
+    std::cerr << "filename is required, count of args must be 1\n";
+    return 1;
+  }
 
-  while (std::cin >> current) {
-    try {
-      if (cmds.find(current) == cmds.end()) {
-        throw std::logic_error("Unknown command");
+  std::ifstream file(argv[1]);
+  if (!file) {
+    std::cerr << "could not open file\n";
+    return 1;
+  }
+
+  std::istream & input = std::cin;
+  std::ostream & output = std::cout;
+
+  levkin::Graphs graphs;
+
+  std::string graph_name;
+  while (file >> graph_name) {
+    graphs.createGraph(graph_name);
+    size_t edges_count = 0;
+    if (!(file >> edges_count)) {
+      break;
+    }
+    for (size_t i = 0; i < edges_count; ++i) {
+      std::string start, end;
+      size_t weight = 0;
+      if (file >> start >> end >> weight) {
+        graphs.addEdge(graph_name, start, end, weight);
       }
-      cmds.at(current)(std::cin, std::cout, db);
-    } catch (const std::exception& e) {
-      std::cout << "<INVALID COMMAND>\n";
-      std::string dummy;
-      std::getline(std::cin, dummy);
     }
   }
 
-  if (!std::cin.eof() && std::cin.fail()) {
-    std::cerr << "Bad input\n";
+  levkin::HashTable< std::string, levkin::cmd_t, levkin::HasherXx< std::string >, levkin::KeyComparator > cmds(64);
+  cmds.add("graphs", levkin::cmdGraphs);
+  cmds.add("vertexes", levkin::cmdVertexes);
+  cmds.add("outbound", levkin::cmdOutbound);
+  cmds.add("inbound", levkin::cmdInbound);
+  cmds.add("bind", levkin::cmdBind);
+  cmds.add("cut", levkin::cmdCut);
+  cmds.add("create", levkin::cmdCreate);
+  cmds.add("merge", levkin::cmdMerge);
+  cmds.add("extract", levkin::cmdExtract);
+
+  std::streamsize max_streamsize = std::numeric_limits< std::streamsize >::max();
+  std::string cmd;
+  
+  while (input >> cmd) {
+    try {
+      cmds.at(cmd)(input, output, graphs);
+      if (input.fail()) {
+        output << "<INVALID COMMAND>\n";
+        input.clear();
+        input.ignore(max_streamsize, '\n');
+      }
+    } catch (const std::out_of_range & e) {
+      output << "<INVALID COMMAND>\n";
+      input.clear();
+      input.ignore(max_streamsize, '\n');
+    }
+  }
+
+  if (!input.eof() && input.fail()) {
+    std::cerr << "bad input\n";
     return 1;
   }
+
   return 0;
 }

e829893a930e88f71d8b16428df0d8a4b9108e0b
Dmitry Levkin <mit.levkin@vk.com>
2026-05-23 16:36:56 +0300

    Add unit tests for HashTable implementation

---
+++ levkin.dima/S3/test-hashtable.cpp
@@ -0,0 +1,135 @@
+#include <boost/test/tools/old/interface.hpp>
+#include <boost/test/unit_test.hpp>
+#include "hashtable.hpp" 
+#include <string>
+#include <functional>
+#include <stdexcept>
+
+using namespace levkin;
+
+struct SimpleStringHash {
+  size_t operator()(const std::string& s) const {
+    return std::hash<std::string>{}(s);
+  }
+};
+
+struct SimpleStringEqual {
+  bool operator()(const std::string& a, const std::string& b) const {
+    return a == b;
+  }
+};
+
+using TestTable = HashTable<std::string, int, SimpleStringHash, SimpleStringEqual>;
+
+BOOST_AUTO_TEST_CASE(ht_constructor_test)
+{
+  TestTable ht(8);
+  BOOST_CHECK_EQUAL(ht.has("key"), false);
+}
+
+BOOST_AUTO_TEST_CASE(ht_add_and_has_test)
+{
+  TestTable ht(4);
+  
+  ht.add("apple", 100);
+  ht.add("banana", 200);
+
+  BOOST_CHECK(ht.has("apple"));
+  BOOST_CHECK(ht.has("banana"));
+  BOOST_CHECK(!ht.has("orange"));
+}
+
+BOOST_AUTO_TEST_CASE(ht_update_value_test)
+{
+  TestTable ht(4);
+  
+  ht.add("key1", 10);
+  ht.add("key1", 20); 
+
+  BOOST_CHECK(ht.has("key1"));
+  
+  BOOST_CHECK_EQUAL(ht.drop("key1"), 20);
+  BOOST_CHECK(!ht.has("key1"));
+}
+
+BOOST_AUTO_TEST_CASE(ht_overflow_bucket_test)
+{
+  struct CollisionHash {
+    size_t operator()(const std::string&) const { return 0; }
+  };
+  using CollisionTable = HashTable<std::string, int, CollisionHash, SimpleStringEqual>;
+
+  CollisionTable ht(4);
+
+  ht.add("one", 1);
+  ht.add("two", 2);
+  ht.add("three", 3);
+
+  ht.add("four", 4);
+
+  BOOST_CHECK(ht.has("one"));
+  BOOST_CHECK(ht.has("two"));
+  BOOST_CHECK(ht.has("three"));
+  BOOST_CHECK(ht.has("four"));
+
+  BOOST_CHECK_EQUAL(ht.get_overflow_count(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(ht_drop_test)
+{
+  TestTable ht(8);
+  
+  ht.add("test", 42);
+  BOOST_CHECK(ht.has("test"));
+
+  int removed = ht.drop("test");
+  BOOST_CHECK_EQUAL(removed, 42);
+  BOOST_CHECK(!ht.has("test"));
+
+  BOOST_CHECK_THROW(ht.drop("unknown"), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(ht_drop_from_overflow_test)
+{
+  struct CollisionHash {
+    size_t operator()(const std::string&) const { return 0; }
+  };
+  using CollisionTable = HashTable<std::string, int, CollisionHash, SimpleStringEqual>;
+
+  CollisionTable ht(4);
+  ht.add("a", 1);
+  ht.add("b", 2);
+  ht.add("c", 3);
+  ht.add("d", 4);
+
+  BOOST_CHECK_EQUAL(ht.drop("d"), 4);
+  BOOST_CHECK(!ht.has("d"));
+  BOOST_CHECK(ht.has("a"));
+}
+
+BOOST_AUTO_TEST_CASE(ht_rehash_test)
+{
+  TestTable ht(2);
+  
+  ht.add("k1", 10);
+  ht.add("k2", 20);
+  ht.add("k3", 30);
+
+  ht.rehash(16);
+
+  BOOST_CHECK(ht.has("k1"));
+  BOOST_CHECK(ht.has("k2"));
+  BOOST_CHECK(ht.has("k3"));
+}
+
+BOOST_AUTO_TEST_CASE(ht_metrics_test)
+{
+  TestTable ht(5);
+  
+  BOOST_CHECK_CLOSE(ht.get_average_elements_per_bucket(), 0.0, 0.001);
+
+  ht.add("1", 10);
+  ht.add("2", 20);
+  
+  BOOST_CHECK_CLOSE(ht.get_average_elements_per_bucket(), 0.4, 0.001);
+}
